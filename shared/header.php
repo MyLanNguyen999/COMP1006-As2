4d5a194ce2abf6c668e469e9ee468e1e8c57697d
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-27 19:23:42 -0400

    update header.php

--- shared/header.php
+++ shared/header.php
@@ -65,21 +65,6 @@
                     </li>
                     ';
             }
-
-            // // * retrieve the list of the pages
-            // include('shared/db.php');
-            // $sql = "SELECT pageId, title FROM adminPages";
-            // $cmd = $db->prepare($sql);
-            // $cmd->execute();
-            // $pages = $cmd->fetchAll();
-
-            // // * display each page in the navigation
-            // foreach ($pages as $page) {
-            //     echo '<li><a href="show-page.php?pageId=' . $page['pageId'] . '">' . $page['title'] . '</a></li>';
-            // }
-
-            // // * disconnect
-            // $db = null;
             ?>
     </ul>    
 <main>
