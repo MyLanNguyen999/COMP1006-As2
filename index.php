0a3fba03d5cf637e636306ad6f7e793efbe044bd
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-28 00:19:06 -0400

    add home image to index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include('shared/header.php');
 ?>
 <h1> COMP 1006 - As2 </h1>
 <p> Created by MyLan Nguyen - 200567763</p>
+<img src="img/image.png" alt="homeImage" id="homeImage"/>
 </main>
 </body>
 <?php
