d40e3ebfee7c9bc2b85f050a6594cdf31c179565
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-03-27 21:37:07 -0400

    fix the bug on show-page.php

--- show-page.php
+++ show-page.php
@@ -2,33 +2,44 @@
 // $title = 'Show Page';
 // include('shared/header.php');
 
-// * connect db
-include('shared/db.php');
-
 // * get $pageId
 $pageId = $_GET['pageId'];
 
-    // Retrieve the page information from the database based on the pageId
-    $sql = "SELECT title, content FROM adminPages WHERE pageId = :pageId";
-    $cmd = $db->prepare($sql);
-    $cmd->bindParam(':pageId', $pageId, PDO::PARAM_INT);
-    $cmd->execute();
-    $page = $cmd->fetch();
+// * error handling
+try {
+    // * connect db
+    include('shared/db.php');
 
-    // Display the page information if found
-    if ($page) {
-        $title = $page['title'];
-        include('shared/header.php');
-        echo '<h2>' . $page['title'] . '</h2>';
-        echo '<p>' . $page['content'] . '</p>';
-        include('shared/footer.php');
-    } else {
-        include('shared/header.php');
-        echo '<p>Page not found</p>';
-        include('shared/footer.php');
-    }
+        // * retrieve the page information from the database based on the pageId
+        $sql = "SELECT title, content FROM adminPages WHERE pageId = :pageId";
+        $cmd = $db->prepare($sql);
+        $cmd->bindParam(':pageId', $pageId, PDO::PARAM_INT);
+        $cmd->execute();
+        $page = $cmd->fetch();
+
+         // Debugging output
+    // var_dump($page);
+
+        // * show content
+        if ($page) {
+            $title = $page['title'];
+            $content = $page['content'];
+            include('shared/header.php');
+            echo '<h2>' . $title . '</h2>';
+            echo '<p>' . $content . '</p>';
+            include('shared/footer.php');
+        } else {
+            include('shared/header.php');
+            echo '<p>Page not found</p>';
+            include('shared/footer.php');
+        }
 
 
-// Disconnect from the database
-$db = null;
+    // * disconnect 
+    $db = null;
+}
+catch (Exception $err) {
+    header('location:error.php');
+    exit();
+}
 ?>
